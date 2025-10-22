c38421335274e54d709decbbb6f72fb389f30f3e
shirley.x <shirleyx38@hotmail.com>
2025-10-22 19:47:06 +1100

    Feat: configuring astro deployment to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  site: 'https://shirley543.github.io',
+  base: '/personal-site',
 });
